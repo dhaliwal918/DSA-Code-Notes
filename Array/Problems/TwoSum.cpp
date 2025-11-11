46b26273aa3a53c09fb9e2ccd0bf12e395c3f90a
Sukhmeet Singh Dhaliwal <144631710+dhaliwal918@users.noreply.github.com>
2025-11-11 14:16:40 +0530

    new files

--- Array/Problems/TwoSum.cpp
+++ Array/Problems/TwoSum.cpp
@@ -6,23 +6,32 @@
 
 using namespace std;
 
-// Pending
-
 vector<int> twoSum(vector<int> &nums, int target)
 {
-    sort(nums.begin(), nums.end());
-    int x = 0, y = 1;
-    for (int i = 1; i < nums.size(); i++)
-    {   
-        if(nums[x] + nums[y] < target){
-            y++;
-        }else if(nums[x] + nums[y] > target){
-            x++;
-        }else{
-            return {x,y};
+    vector<int> pair;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        bool found = false;
+        for (int j = 0; j < nums.size(); j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            if (nums[i] + nums[j] == target)
+            {
+                pair.push_back(i);
+                pair.push_back(j);
+                found = true;
+                break;
+            }
+        }
+        if (found)
+        {
+            break;
         }
     }
-    
+    return pair;
 }
 
 int main()
